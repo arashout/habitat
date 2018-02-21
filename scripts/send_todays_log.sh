78c165162615c335439ee78891226cfbc4622298
Arash Outadi <arash.outadi@ravelin.com>
2018-02-21 18:57:02 +0000

    Get rid of function

--- scripts/send_todays_log.sh
+++ scripts/send_todays_log.sh
@@ -1,19 +1,16 @@
-send_todays_log(){
-    now=`date +"%Y-%m-%d"`
-    filepath=~/Documents/Logs/$now-log.md
-    markdown_string=`cat ${filepath}`
+now=`date +"%Y-%m-%d"`
+filepath=~/Documents/Logs/$now-log.md
+markdown_string=`cat ${filepath}`
 
-    export TODAYS_LOG_MD=$markdown_string
-    # TODO: Write custom Python script that parses Github Markdown to Slack markdown
-    curl --trace-ascii dump.txt -X POST -H "Authorization: Bearer ${SLACK_API_TOKEN}" \
-    -H 'Content-type: application/json' \
-    --data "$(python -c 'import json, os, sys; print json.dumps({
-        "channel": os.getenv("SLACK_RAVELIN_ME"),
-        "text": os.getenv("TODAYS_LOG_MD"),
-    })')" \
-    https://slack.com/api/chat.postMessage
-    unset TODAYS_LOG_MD
+export TODAYS_LOG_MD=$markdown_string
+# TODO: Write custom Python script that parses Github Markdown to Slack markdown
+curl --trace-ascii dump.txt -X POST -H "Authorization: Bearer ${SLACK_API_TOKEN}" \
+-H 'Content-type: application/json' \
+--data "$(python -c 'import json, os, sys; print json.dumps({
+    "channel": os.getenv("SLACK_RAVELIN_ME"),
+    "text": os.getenv("TODAYS_LOG_MD"),
+})')" \
+https://slack.com/api/chat.postMessage
+unset TODAYS_LOG_MD
 
-    echo # To create a new line after curl
-    
-}
+echo # To create a new line after curl
