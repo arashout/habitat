b1347a87552d74859bc90d449c0a02a7f1f8aa20
Arash Outadi <arash.outadi@ravelin.com>
2018-03-05 17:36:31 +0000

    Change to python3 and format better

--- scripts/send_todays_log.sh
+++ scripts/send_todays_log.sh
@@ -6,10 +6,13 @@ export TODAYS_LOG_MD=$markdown_string
 # TODO: Write custom Python script that parses Github Markdown to Slack markdown
 curl -X POST -H "Authorization: Bearer ${SLACK_API_TOKEN}" \
 -H 'Content-type: application/json' \
---data "$(python -c 'import json, os, sys; print json.dumps({
-    "channel": os.getenv("SLACK_RAVELIN_ME"),
-    "text": os.getenv("TODAYS_LOG_MD"),
-})')" \
+--data "$(python3 -c 'import json, os, sys; print(
+    json.dumps({
+        "channel": os.getenv("SLACK_RAVELIN_ME"),
+        "text": os.getenv("TODAYS_LOG_MD"),
+        })
+    )'
+    )" \
 https://slack.com/api/chat.postMessage
 unset TODAYS_LOG_MD
 
